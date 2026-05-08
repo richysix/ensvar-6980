f1b80e9c9d647696265310b6eba2db09efd55164
Richard White <rich.white@cantab.net>
2026-05-08 14:18:37 +0100

    time-mem-plot.R: Add line graph summarising fold change
    Add line graph summarising fold change in runtime given number
    of forks facetted by options and dataset type.
    Add theme_minimal to all plots.

--- time-mem-plot.R
+++ time-mem-plot.R
@@ -43,27 +43,22 @@ load_data_file <- function(filename) {
 }
 
 convert_to_mins <- function(vec) {
-    # second_string <- "[0-9]+s"
-    # seconds <- str_extract(vec, second_string) |> 
-    #     str_remove(pattern = "s") |> 
-    #     as.integer() |> 
-    #     replace_na(replace = 0)
     minute_string <- "[0-9]+m"
-    minutes <- str_extract(vec, minute_string) |> 
-        str_remove(pattern = "m") |> 
-        as.integer() |> 
+    minutes <- str_extract(vec, minute_string) |>
+        str_remove(pattern = "m") |>
+        as.integer() |>
         replace_na(replace = 0)
     hour_string <- "[0-9]+h"
-    hours <- str_extract(vec, hour_string) |> 
-        str_remove(pattern = "h") |> 
-        as.integer() |> 
+    hours <- str_extract(vec, hour_string) |>
+        str_remove(pattern = "h") |>
+        as.integer() |>
         replace_na(replace = 0)
     return(hours * 60 + minutes)
 }
 
 # load exome data file
 exome_data <- load_data_file(
-    file.path(cmd_line_args$args[1], "exome", "reports", "exome-results.tsv")) |> 
+    file.path(cmd_line_args$args[1], "exome", "reports", "exome-results.tsv")) |>
     mutate(
         `Runtime (Minutes)` = convert_to_mins(`Job Wall-clock time`),
         buffer_size = factor(buffer_size, levels = c("5000", "10000", "50000", "100000")),
@@ -83,16 +78,17 @@ output_plot <- function(plot_obj, file_name) {
 }
 
 time_plot_by_options <- function(data_df) {
-    plot <- data_df |> 
+    plot <- data_df |>
         ggplot() +
         aes(x = buffer_size, y = `Runtime (Minutes)`, fill = forks) +
         geom_boxplot(outliers = FALSE) +
         scale_fill_manual(values = biovisr::cbf_palette(nlevels(data_df$forks))) +
         scale_x_discrete(name = "Buffer size") +
-        facet_wrap(vars(options))
+        facet_wrap(vars(options)) +
+        theme_minimal()
     return(plot)
 }
-time_plot_by_options(exome_data) |> 
+time_plot_by_options(exome_data) |>
     output_plot(file_name = 
         file.path(
             cmd_line_args$args[1], 
@@ -102,16 +98,17 @@ time_plot_by_options(exome_data) |>
     )
 
 time_plot_by_forks <- function(data_df) {
-    plot <- data_df |> 
+    plot <- data_df |>
         ggplot() +
         aes(x = buffer_size, y = `Runtime (Minutes)`, fill = options) +
         geom_boxplot(outliers = FALSE) +
         scale_fill_manual(values = biovisr::cbf_palette(nlevels(data_df$options))) +
         scale_x_discrete(name = "Buffer size") +
-        facet_wrap(vars(forks))
+        facet_wrap(vars(forks), labeller = label_both) +
+        theme_minimal()
     return(plot)
 }
-time_plot_by_forks(exome_data) |> 
+time_plot_by_forks(exome_data) |>
     output_plot(file_name = 
         file.path(
             cmd_line_args$args[1], 
@@ -121,16 +118,17 @@ time_plot_by_forks(exome_data) |>
     )
 
 mem_plot_by_options <- function(data_df) {
-    plot <- data_df |> 
+    plot <- data_df |>
         ggplot() +
         aes(x = buffer_size, y = `Memory Utilized (GB)`, fill = forks) +
         geom_boxplot(outliers = FALSE) +
         scale_fill_manual(values = biovisr::cbf_palette(nlevels(data_df$forks))) +
         scale_x_discrete(name = "Buffer size") +
-        facet_wrap(vars(options))
+        facet_wrap(vars(options)) +
+        theme_minimal()
     return(plot)
 }
-mem_plot_by_options(exome_data) |> 
+mem_plot_by_options(exome_data) |>
     output_plot(file_name = 
         file.path(
             cmd_line_args$args[1], 
@@ -140,16 +138,17 @@ mem_plot_by_options(exome_data) |>
     )
 
 mem_plot_by_forks <- function(data_df) {
-    plot <- data_df |> 
+    plot <- data_df |>
         ggplot() +
         aes(x = buffer_size, y = `Memory Utilized (GB)`, fill = options) +
         geom_boxplot(outliers = FALSE) +
         scale_fill_manual(values = biovisr::cbf_palette(nlevels(data_df$options))) +
         scale_x_discrete(name = "Buffer size") +
-        facet_wrap(vars(forks))
+        facet_wrap(vars(forks), labeller = label_both) +
+        theme_minimal()
     return(plot)
 }
-mem_plot_by_forks(exome_data) |> 
+mem_plot_by_forks(exome_data) |>
     output_plot(file_name = 
         file.path(
             cmd_line_args$args[1], 
@@ -162,7 +161,7 @@ mem_plot_by_forks(exome_data) |>
 # Genome data
 # load exome data file
 genome_data <- load_data_file(
-    file.path(cmd_line_args$args[1], "genome", "reports", "genome-results.tsv")) |> 
+    file.path(cmd_line_args$args[1], "genome", "reports", "genome-results.tsv")) |>
     mutate(
         `Runtime (Minutes)` = convert_to_mins(`Job Wall-clock time`),
         buffer_size = factor(buffer_size, levels = c("5000", "10000", "50000", "100000")),
@@ -170,7 +169,7 @@ genome_data <- load_data_file(
         forks = factor(forks, levels = c("1", "8", "24", "48"))
     )
 
-time_plot_by_options(genome_data) |> 
+time_plot_by_options(genome_data) |>
     output_plot(file_name = 
         file.path(
             cmd_line_args$args[1], 
@@ -179,7 +178,7 @@ time_plot_by_options(genome_data) |>
             "genome-runtime-by-options-plot.png")
     )
 
-time_plot_by_forks(genome_data) |> 
+time_plot_by_forks(genome_data) |>
     output_plot(file_name = 
         file.path(
             cmd_line_args$args[1], 
@@ -188,9 +187,9 @@ time_plot_by_forks(genome_data) |>
             "genome-runtime-by-forks-plot.png")
     )
 
-genome_data |> 
+genome_data |>
     filter(forks != 1) |>
-    time_plot_by_forks() |> 
+    time_plot_by_forks() |>
     output_plot(file_name = 
         file.path(
             cmd_line_args$args[1], 
@@ -199,7 +198,7 @@ genome_data |>
             "genome-runtime-by-forks-gt1-plot.png")
     )
 
-mem_plot_by_options(genome_data) |> 
+mem_plot_by_options(genome_data) |>
     output_plot(file_name = 
         file.path(
             cmd_line_args$args[1], 
@@ -208,7 +207,7 @@ mem_plot_by_options(genome_data) |>
             "genome-memory-by-options-plot.png")
     )
 
-mem_plot_by_forks(genome_data) |> 
+mem_plot_by_forks(genome_data) |>
     output_plot(file_name = 
         file.path(
             cmd_line_args$args[1], 
@@ -220,7 +219,7 @@ mem_plot_by_forks(genome_data) |>
 ##############
 # Structural variant data
 sv_data <- load_data_file(
-    file.path(cmd_line_args$args[1], "sv", "reports", "sv-results.tsv")) |> 
+    file.path(cmd_line_args$args[1], "sv", "reports", "sv-results.tsv")) |>
     mutate(
         `Runtime (Minutes)` = convert_to_mins(`Job Wall-clock time`),
         buffer_size = factor(buffer_size, levels = c("250", "1250", "2500", "5000", "10000")),
@@ -228,7 +227,7 @@ sv_data <- load_data_file(
         forks = factor(forks, levels = c("1", "8", "16", "24", "48"))
     )
 
-time_plot_by_options(sv_data) |> 
+time_plot_by_options(sv_data) |>
     output_plot(file_name = 
         file.path(
             cmd_line_args$args[1], 
@@ -243,8 +242,8 @@ move_legend <- function(plot_obj) {
             theme(legend.position = c(0.8, 0.3))
     )
 }
-time_plot_by_forks(sv_data) |> 
-    move_legend() |> 
+time_plot_by_forks(sv_data) |>
+    move_legend() |>
     output_plot(file_name = 
         file.path(
             cmd_line_args$args[1], 
@@ -253,9 +252,9 @@ time_plot_by_forks(sv_data) |>
             "sv-runtime-by-forks-plot.png")
     )
 
-sv_data |> 
+sv_data |>
     filter(forks != 1) |>
-    time_plot_by_forks() |> 
+    time_plot_by_forks() |>
     output_plot(file_name = 
         file.path(
             cmd_line_args$args[1], 
@@ -264,7 +263,7 @@ sv_data |>
             "sv-runtime-by-forks-gt1-plot.png")
     )
 
-mem_plot_by_options(sv_data) |> 
+mem_plot_by_options(sv_data) |>
     output_plot(file_name = 
         file.path(
             cmd_line_args$args[1], 
@@ -273,8 +272,8 @@ mem_plot_by_options(sv_data) |>
             "sv-memory-by-options-plot.png")
     )
 
-mem_plot_by_forks(sv_data) |> 
-    move_legend() |> 
+mem_plot_by_forks(sv_data) |>
+    move_legend() |>
     output_plot(file_name = 
         file.path(
             cmd_line_args$args[1], 
@@ -283,13 +282,56 @@ mem_plot_by_forks(sv_data) |>
             "sv-memory-by-forks-plot.png")
     )
 
+# Plot runtime speed up
+# load summary files
+load_summary_file <- function(type, options) {
+    file_name <- file.path(
+        cmd_line_args$args[1], 
+        type, 
+        "reports", 
+        glue::glue("{type}-time-delta-{options}-results.tsv")
+    )
+    df <- read_tsv(
+            file_name, 
+            col_types = cols(
+                variant_type = col_factor(),
+                buffer_size = col_factor(levels = as.character(as.integer(c(250, 1250, 2500, 5000, 10000, 50000, 100000)))),
+                forks = col_factor(levels = as.character(c(1, 8, 16, 24, 48))),
+                options = col_factor()
+            )
+        )
+    return(df)
+}
+params <- expand_grid(type = c("exome", "genome", "sv"), options = c("None", "everything"))
+
+all_data <- map2(params$type, params$options, load_summary_file) |>
+    list_rbind()
+
+line_colours <- biovisr::cbf_palette(x = 8)[c(1:3,5:8)]
+summary_plot <- all_data |>
+    ggplot() +
+    aes(
+        x = forks,
+        y = `x Speed up`,
+        colour = buffer_size,
+        group = interaction(variant_type, buffer_size)
+    ) +
+    geom_line(linewidth = 1) +
+    geom_point() +
+    scale_color_manual(values = line_colours) +
+    facet_grid(rows = vars(variant_type), cols = vars(options)) +
+    theme_minimal() +
+    theme(strip.background = element_rect(fill = "#cccccc"))
+
+output_plot(summary_plot, file.path(cmd_line_args$args[1], "time-fold-change.png"))
+
 # AUTHOR
 #
 # Richard White <rich.white@cantab.net>
 #
 # COPYRIGHT AND LICENSE
 #
-# This software is Copyright (c) 2025 EMBL-EBI
+# This software is Copyright (c) 2026 EMBL-European Bioinformatics Institute
 #
 # This is free software, licensed under:
 #
